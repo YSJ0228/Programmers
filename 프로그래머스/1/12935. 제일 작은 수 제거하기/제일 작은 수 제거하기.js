c11c3e3f9c9d4189f47f71c66265e25979e4803f
YSJ0228 <165990457+YSJ0228@users.noreply.github.com>
2025-10-28 16:22:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.19 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    if (arr.length === 1) return [-1];
+
+    const min = Math.min(...arr);
+    
+    const answer = arr.filter(num => num !== min);
+    
+    return answer;
+}
